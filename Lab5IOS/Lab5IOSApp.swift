30ca475208d5cff2856cda995f52d98c818ef171
SSviatoslaWW <sviatoslaw7779@gmail.com>
2025-10-28 07:28:46 +0200

    Initial Commit

---
+++ Lab5IOS/Lab5IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5IOSApp.swift
+//  Lab5IOS
+//
+//  Created by Svituk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab5IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab5IOSTests/Lab5IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5IOSTests.swift
+//  Lab5IOSTests
+//
+//  Created by Svituk on 28.10.2025.
+//
+
+import Testing
+@testable import Lab5IOS
+
+struct Lab5IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
